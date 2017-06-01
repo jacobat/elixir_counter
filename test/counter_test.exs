782922a9f58717970ef2e72ec284061d77a3d3b6
Benjamin Quorning <benjamin@quorning.net>
2017-06-01 13:44:37 +0200

    Extract test setup

--- test/counter_test.exs
+++ test/counter_test.exs
@@ -2,21 +2,23 @@ defmodule CounterTest do
   use ExUnit.Case, async: true
   doctest Counter
 
-  test "is zero at first" do
-    {:ok, pid} = Counter.start_link
+  setup do
+    {:ok, counter} = Counter.start_link
+    {:ok, counter: counter}
+  end
+
+  test "is zero at first", %{counter: pid} do
     assert Counter.get(pid) == 0
   end
 
-  test "can be incremented to one" do
-    {:ok, pid} = Counter.start_link
+  test "can be incremented to one", %{counter: pid} do
     assert Counter.get(pid) == 0
 
     Counter.increment(pid)
     assert Counter.get(pid) == 1
   end
 
-  test "can also be decremented" do
-    {:ok, pid} = Counter.start_link
+  test "can also be decremented", %{counter: pid} do
     assert Counter.get(pid) == 0
 
     Counter.increment(pid)
